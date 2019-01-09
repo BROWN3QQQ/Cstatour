6dd258d6a44ab0cb25dfb1548b3bfa49ffd5b247
BROWN3QQQ <603856170@qq.com>
2019-01-09 17:58:24 +0800

    chage

--- src/main/java/com/brown3qqq/cstatour/controller/ColumnCategoryController.java
+++ src/main/java/com/brown3qqq/cstatour/controller/ColumnCategoryController.java
@@ -1,15 +1,29 @@
 package com.brown3qqq.cstatour.controller;
 
 
+import com.brown3qqq.cstatour.pojo.example;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class ColumnCategoryController {
 
+    @Autowired
+    MongoTemplate mongoTemplate;
+
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     public String test(){
+
+        example e = new example("10001","sad");
+
+        mongoTemplate.save(e,"user");
+        System.out.println("mongoDB插入数据成功,集合为user,文档为："+mongoTemplate.getCollection("user"));
         return "hello world!　烦人的世界, 热更新测试";
     }
 }
--- src/main/java/com/brown3qqq/cstatour/dao/testmongo.java
+++ src/main/java/com/brown3qqq/cstatour/dao/testmongo.java
@@ -1,4 +1,17 @@
 package com.brown3qqq.cstatour.dao;
 
+import com.mongodb.DB;
+import com.mongodb.Mongo;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Set;
+
+/*测试 spring-data-mongo
+* */
+
+
+
 public class testmongo {
+
+
 }
